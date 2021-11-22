49ce59e207d41010fff82f243030930acf49bcad
nimamac <1132957271@qq.com>
2021-11-22 15:44:20 +0800

    first commit init all

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.$u.get('/api/index').then(res => {
+				console.log(res)
+			}).catch(e => {
+				// console.log(e)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
